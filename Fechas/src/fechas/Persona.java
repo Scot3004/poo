967dec4cc94b2f3dd265fc9333572b7b009f4f82
scot3004 <scot3004@Scot3004-PC>
2014-10-10 18:44:29 -0500

    Creada clase Persona

---
+++ Fechas/src/fechas/Persona.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fechas;
+
+import java.util.Date;
+
+/**
+ *
+ * @author scot3004
+ */
+public class Persona {
+    protected String nombre;
+    protected Date nacimiento;
+    protected String direccion;
+    protected String codigoPostal;
+    protected String ciudad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(Date nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    
+}
